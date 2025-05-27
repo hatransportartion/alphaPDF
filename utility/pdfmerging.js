6b38ebc58589166eac4d5c5949c3d35bc7e8b0ab
Sandeep Singh <sandeep2197@Sandeeps-MacBook-Air.local>
2025-05-27 16:37:56 -0700

    fixed pdf merging issue

--- utility/pdfmerging.js
+++ utility/pdfmerging.js
@@ -14,19 +14,18 @@ async function mergePDFs(pdfPaths, outputPath) {
     // const URL = pdfPath.url.replace("http://", "https://");
     const imageBytes = await fetch(pdfPath.url, {
       method: "GET",
-    }).then((res) => res.arrayBuffer()).catch((err) => {
-      console.error("Error fetching PDF:", err);
-      throw new Error("Error fetching PDF");
-    }
-    );
+    })
+      .then((res) => res.arrayBuffer())
+      .catch((err) => {
+        console.error("Error fetching PDF:", err);
+        throw new Error("Error fetching PDF");
+      });
     console.log(imageBytes);
 
-    if (
-      pdfPath.type === "application/pdf" ||
-      pdfPath.type === "image/pdf"
-    ) {
-      
-      const pdfDocToMerge = await PDFDocument.load(imageBytes);
+    if (pdfPath.type === "application/pdf" || pdfPath.type === "image/pdf") {
+      const pdfDocToMerge = await PDFDocument.load(imageBytes, {
+        ignoreEncryption: true,
+      });
       const copiedPages = await pdfDoc.copyPages(
         pdfDocToMerge,
         pdfDocToMerge.getPageIndices()
@@ -34,10 +33,7 @@ async function mergePDFs(pdfPaths, outputPath) {
       copiedPages.forEach((page) => {
         pdfDoc.addPage(page);
       });
-    } else if (
-      pdfPath.type === "image/jpeg" ||
-      pdfPath.type === "image/jpg"
-    ) {
+    } else if (pdfPath.type === "image/jpeg" || pdfPath.type === "image/jpg") {
       const jpgImageBytes = await pdfDoc.embedJpg(imageBytes);
       const jpgDims = jpgImageBytes.scale(0.5);
       const page = pdfDoc.addPage([jpgDims.width, jpgDims.height]);
@@ -75,10 +71,10 @@ async function mergeAndSavePDFs(pdfPaths) {
     console.log("Merging and saving PDFs -->> ");
     const fileName = generateUniqueFilename();
     let outputFilePath = `/home/app/docs/${fileName}.pdf`;
-    const NODE_ENV = process.env.NODE_ENV || 'local';
+    const NODE_ENV = process.env.NODE_ENV || "local";
     console.log("NODE_ENV: ", NODE_ENV);
     console.log("Output File Path: ", outputFilePath);
-    if(NODE_ENV === 'local') {
+    if (NODE_ENV === "local") {
       outputFilePath = `PDFs/${fileName}.pdf`;
     }
     console.log("Output File Path: ", outputFilePath);
